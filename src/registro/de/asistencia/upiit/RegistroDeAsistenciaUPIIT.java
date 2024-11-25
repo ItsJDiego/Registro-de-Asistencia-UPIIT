c62408d8ca1e13654e2297e79a14c806cda5a152
JuanDi <jdiegoespi@gmail.com>
2024-11-24 22:00:21 -0600

    Primer Commit

---
+++ src/registro/de/asistencia/upiit/RegistroDeAsistenciaUPIIT.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package registro.de.asistencia.upiit;
+
+/**
+ *
+ * @author User_Asus
+ */
+public class RegistroDeAsistenciaUPIIT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
